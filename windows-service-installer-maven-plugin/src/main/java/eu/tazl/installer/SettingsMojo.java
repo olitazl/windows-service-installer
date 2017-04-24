c5ab9b8b6ceef6a7210904ba66defc824f2e0a1f
Oliver Tazl <oliver.tazl@gmail.com>
2017-04-24 12:47:49 +0200

    dep cleanup and restructuring

--- windows-service-installer-maven-plugin/src/main/java/eu/tazl/installer/SettingsMojo.java
+++ windows-service-installer-maven-plugin/src/main/java/eu/tazl/installer/SettingsMojo.java
@@ -3,7 +3,6 @@ package eu.tazl.installer;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
